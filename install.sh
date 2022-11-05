30e6b5bc471d4e55ad4178f6390a05b8f9dce161
PikaProgram <61467599+PikaProgram@users.noreply.github.com>
2022-11-06 06:35:07 +0700

    Add NVM and colorls to install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ install_packages() {
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/jenv/jenv.git ~/.jenv
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+    
+    gem install colorls
 }
 
 clone_dotfiles() {
